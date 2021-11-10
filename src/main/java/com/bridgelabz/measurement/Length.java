20c4da35cf0cf5374c8a7ab9139550c9c11db6ea
unknown <you@example.com>
2021-11-10 14:55:20 +0530

    Added: Code and test(passed) case to compare 2 feet value

--- src/main/java/com/bridgelabz/measurement/Length.java
+++ src/main/java/com/bridgelabz/measurement/Length.java
@@ -22,6 +22,8 @@ public class Length {
      */
 
     public boolean compare(Length that) {
+        if(this.unit.equals(Unit.FEET) && that.unit.equals(Unit.FEET))
+            return Double.compare(this.value, that.value) == 0;
         if(this.unit.equals(Unit.FEET) && that.unit.equals(Unit.INCH))
             return Double.compare(this.value*FEET_TOINCH, that.value) == 0;
         return false;
