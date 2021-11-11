43abcee5495adf0b9bedc034a025b9f34a09b4e7
unknown <you@example.com>
2021-11-11 23:12:45 +0530

    Added: Code and test case to add 1Feet and 1Feet and expect output as 24Inch

--- src/test/java/com/bridgelabz/measurement/MeasurementTest.java
+++ src/test/java/com/bridgelabz/measurement/MeasurementTest.java
@@ -188,4 +188,12 @@ public class MeasurementTest {
         assertEquals(expectedSum, actualSum);
     }
 
+    @Test
+    public void given1FeetAnd1Feet_WhenAdded_ShouldReturn24Inch() {
+        Length feet1 = new Length(Length.Unit.FEET, 1.0);
+        Length feet2 = new Length(Length.Unit.FEET, 1.0);
+        Length expectedSum = new Length(Length.Unit.INCH, 24.0);
+        Length actualSum = feet1.sumOfLength(feet2);
+        assertEquals(expectedSum, actualSum);
+    }
 }
