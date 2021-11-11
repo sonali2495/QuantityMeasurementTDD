f16ba4c4e71462610fc37b6e6715b0732283ca17
unknown <you@example.com>
2021-11-12 02:17:29 +0530

    Added: Code and test case to add litre  and mililitre  and expect sum in litres

--- src/test/java/com/bridgelabz/measurement/MeasurementTest.java
+++ src/test/java/com/bridgelabz/measurement/MeasurementTest.java
@@ -303,4 +303,13 @@ public class MeasurementTest {
         Volume actualSum = gallon.sumOfVolume(liter);
         assertEquals(expectedSum, actualSum);
     }
+
+    @Test
+    public void given1LiterAnd1000MilliLiter_WhenAdded_ShouldReturn2Liter() {
+        Volume liter = new Volume(Volume.Unit.LITER, 1.0);
+        Volume ml = new Volume(Volume.Unit.MILLI_LITER, 1000.0);
+        Volume expectedSum = new Volume(Volume.Unit.LITER, 2.0);
+        Volume actualSum = liter.sumOfVolume(ml);
+        assertEquals(expectedSum, actualSum);
+    }
 }
